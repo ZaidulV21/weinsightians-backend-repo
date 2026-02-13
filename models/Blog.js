da4c9efa5b6473307c78bfa26565054c6da3c590
Zaidul Vasf <mhdzedx@gmail.com>
2026-02-13 18:19:59 +0530

    init commit

---
+++ models/Blog.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    image: {
+        type: String,
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Blog", BlogSchema);
